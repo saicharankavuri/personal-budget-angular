b0fa66db2b6a8c7e371e3f26b85f344cbc292be2
Sai Charan Kavuri <saicharankavuri@Sais-Air.lan>
2023-09-23 16:35:20 -0400

    Added Angular

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 // Budget API
-
+const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,29 +7,18 @@ const port = 3000;
 
 app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+
+fs.readFile('data.json', 'utf8', (err, file) => {
+    data = file;
+    return;
+})
 
 app.use(cors({
     origin: '*'
 }));
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.send(data);
 });
 
 app.listen(port, () => {
